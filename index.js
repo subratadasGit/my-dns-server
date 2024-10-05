1b1d0524eb96a0db5efd84c684ef2d9616b89389
Subrata Das <subratadasgit786@gmail.com>
2024-10-06 01:51:58 +0530

    1

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-import { createSocket } from 'node:dgram';
+const dgram = require('node:dgram');
+const server = dgram.createSocket('udp4');
 
-const server = createSocket('udp4');
+server.on('message', (msg, rinfo) => {
+    console.log('Incoming message from', rinfo.address, 'on port', rinfo.port);
+    console.log('Message:', msg.toString());
+
+    // You can respond to the client here
+    const response = Buffer.from('Hello from DNS Server');
+    server.send(response, rinfo.port, rinfo.address, (err) => {
+        if (err) console.error('Error sending response:', err);
+        else console.log('Response sent');
+    });
+});
+
+server.on('error', (err) => {
+    console.error('Server error:', err);
+    server.close();
+});
+
+server.bind(53, () => {
+    console.log('DNS Server is running on port 53');
+});
