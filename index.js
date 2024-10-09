0fba24dcee16b6363e204c455a1de9e17b4b0727
Subrata Das <subratadasgit786@gmail.com>
2024-10-09 23:53:29 +0530

    1

--- index.js
+++ index.js
@@ -3,21 +3,12 @@ const server = dgram.createSocket('udp4');
 
 server.on('message', (msg, rinfo) => {
     console.log('Incoming message from', rinfo.address, 'on port', rinfo.port);
-    console.log('Message:', msg.toString());
+    console.log( 'Message:', msg.toString())});
 
-    // You can respond to the client here
-    const response = Buffer.from('Hello from DNS Server');
-    server.send(response, rinfo.port, rinfo.address, (err) => {
-        if (err) console.error('Error sending response:', err);
-        else console.log('Response sent');
-    });
-});
-
-server.on('error', (err) => {
-    console.error('Server error:', err);
-    server.close();
-});
+// try nslookup 
 
 server.bind(53, () => {
     console.log('DNS Server is running on port 53');
 });
+
+//nslookup @localhost  
